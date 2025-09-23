718a776348c2f101d6f37373f7108366f942254c
study24australia <study24australia@gmail.com>
2025-09-23 20:02:05 +1000

    Updated emailService.ts

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -2,7 +2,7 @@ import emailjs from '@emailjs/browser';
 
 // EmailJS configuration
 const EMAILJS_SERVICE_ID = 'service_r8iabck';
-const EMAILJS_TEMPLATE_ID = 'template_bc6khvq'; // Use your working template ID
+const EMAILJS_TEMPLATE_ID = 'template_lda9ada' //'template_bc6khvq'; // Use your working template ID
 const EMAILJS_PUBLIC_KEY = 'kSW1BdcJIAb9BrX58';
 
 // Initialize EmailJS
